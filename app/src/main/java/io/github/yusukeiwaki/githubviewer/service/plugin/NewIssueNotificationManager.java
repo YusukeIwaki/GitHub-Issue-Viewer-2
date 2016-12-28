87c372f9684ecdf577e69077c917148833aa549d
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2016-12-28 14:01:34 +0900

    notification mod.

--- app/src/main/java/io/github/yusukeiwaki/githubviewer/service/plugin/NewIssueNotificationManager.java
+++ app/src/main/java/io/github/yusukeiwaki/githubviewer/service/plugin/NewIssueNotificationManager.java
@@ -2,15 +2,23 @@ package io.github.yusukeiwaki.githubviewer.service.plugin;
 
 import android.app.Notification;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
 
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
 import java.util.List;
 
 import bolts.Continuation;
 import bolts.Task;
+import bolts.TaskCompletionSource;
 import io.github.yusukeiwaki.githubviewer.R;
 import io.github.yusukeiwaki.githubviewer.model.Issue;
 import io.github.yusukeiwaki.githubviewer.model.internal.SearchIssueProcedure;
@@ -61,13 +69,97 @@ public class NewIssueNotificationManager extends AbstractRealmModelObserver<Sear
         return (int) (procedure.getQueryId() % Integer.MAX_VALUE);
     }
 
-    private Task<Notification> generateNotificationFor(SearchIssueProcedure procedure) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                .setContentTitle(procedure.getQuery().getTitle())
-                .setContentText("new message")
-                .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
-                .setSmallIcon(R.mipmap.ic_launcher);
+    private Task<Notification> generateNotificationFor(final SearchIssueProcedure procedure) {
+        return getIconUrlFrom(procedure.getItems())
+                .onSuccessTask(new Continuation<String, Task<Bitmap>>() {
+                    @Override
+                    public Task<Bitmap> then(Task<String> task) throws Exception {
+                        final float dp = context.getResources().getDisplayMetrics().density;
+                        return getBitmapForUser(task.getResult(), (int) (48*dp));
+                    }
+                })
+                .continueWithTask(new Continuation<Bitmap, Task<Notification>>() {
+                    @Override
+                    public Task<Notification> then(Task<Bitmap> task) throws Exception {
 
-        return Task.forResult(builder.build());
+                        int numUnread = 0;
+                        final long lastSeen = procedure.getQuery().getLastSeenAt();
+                        StringBuilder sb = new StringBuilder();
+                        sb.append(context.getResources().getString(R.string.notification_title_unread_issue));
+                        for (Issue issue : procedure.getItems())  {
+                            if (issue.getUpdated_at().getTime() > lastSeen) {
+                                numUnread++;
+                                sb.append("\n> ").append(issue.getTitle());
+                            }
+                        }
+
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                                .setContentTitle(procedure.getQuery().getTitle())
+                                .setContentText(sb.toString())
+                                .setNumber(numUnread)
+                                .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                                .setSmallIcon(R.drawable.ic_notification);
+
+                        if (!task.isFaulted()) {
+                            builder.setLargeIcon(task.getResult());
+                        }
+
+                        Notification notification = new NotificationCompat.BigTextStyle(builder)
+                                .bigText(sb.toString())
+                                .build();
+
+                        return Task.forResult(notification);
+                    }
+                });
+    }
+
+    private Task<String> getIconUrlFrom(List<Issue> issues) {
+        return Task.forError(new UnsupportedOperationException("Not implemented."));
+    }
+
+    private Task<Bitmap> getBitmapForUser(final String iconUrl, final int size) {
+        final TaskCompletionSource<Bitmap> task = new TaskCompletionSource<>();
+
+        // Picasso can be triggered only on Main Thread.
+        if (Looper.myLooper() != Looper.getMainLooper()) {
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    getBitmapForUser(iconUrl, size).continueWith(new Continuation<Bitmap, Object>() {
+                        @Override
+                        public Object then(Task<Bitmap> _task) throws Exception {
+                            if (_task.isFaulted()) {
+                                task.setError(_task.getError());
+                            } else {
+                                task.setResult(_task.getResult());
+                            }
+                            return null;
+                        }
+                    });
+                }
+            });
+            return task.getTask();
+        }
+
+        Picasso.with(context)
+                .load(iconUrl)
+                .into(new Target() {
+                    @Override
+                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                        if (bitmap != null) {
+                            task.trySetResult(bitmap);
+                        }
+                    }
+
+                    @Override
+                    public void onBitmapFailed(Drawable errorDrawable) {
+                        task.setError(new Exception("onBitmapFailed"));
+                    }
+
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+                    }
+                });
+        return task.getTask();
     }
 }
